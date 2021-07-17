bfbd2f7820746e1ae2bfe0ccb258e64f174b4eb9
mustafa <hamandoshm@hotmail.com>
2021-07-16 22:20:33 -0300

    Add uuid to categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
 
 export const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (req, res) => {
+categoriesRoutes.post('/', (req, res) => {
     const { name, description } = req.body;
-    categories.push({ name, description });
+    const category = { id: uuid(), name, description };
+    categories.push(category);
     return res.status(201).send();
 });
