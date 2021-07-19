104948a6dede972ef45b7e93e410537175c0e689
mustafa <hamandoshm@hotmail.com>
2021-07-19 13:11:33 -0300

    Create use case for category

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,11 +1,11 @@
-import { ICategoryRepository } from '../repositories/ICategoryRepository';
+import { ICategoryRepository } from '../../repositories/ICategoryRepository';
 
 interface IRequest {
     name: string;
     description: string;
 }
 
-export class CreateCategoryService {
+export class CreateCategoryUseCase {
     constructor(private categoryRepository: ICategoryRepository) {}
 
     execute({ name, description }: IRequest): void {
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,13 @@
 import { Router } from 'express';
 
-import { CategoryRepository } from '../modules/cars/repositories/CategoryRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 export const categoriesRoutes = Router();
 
-const categoryRepository = new CategoryRepository();
-const createCategoryService = new CreateCategoryService(categoryRepository);
-
 categoriesRoutes.post('/', (req, res) => {
-    const { name, description } = req.body;
-    createCategoryService.execute({ name, description });
-    return res.status(201).send();
+    return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get('/', (req, res) => {
-    return res.json({ categories: categoryRepository.list() });
+    // return res.json({ categories: categoryRepository.list() });
 });
