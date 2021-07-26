05410c4b97272afe7839ca318004e911c038568e
mustafa <hamandoshm@hotmail.com>
2021-07-26 20:54:32 -0300

    Create use case and controller to user

---
+++ src/modules/accounts/useCases/user/createUser/CreateUserController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+export class CreateUserController {
+    async handle(req: Request, res: Response): Promise<Response> {
+        const { name, email, password, username, driver_license } = req.body;
+        const userRepository = container.resolve(CreateUserUseCase);
+        await userRepository.execute({
+            name,
+            email,
+            password,
+            username,
+            driver_license,
+        });
+        return res.status(201).send();
+    }
+}
---
+++ src/modules/accounts/useCases/user/createUser/CreateUserUseCase.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUserDTO } from '../../../dtos/ICreateUserDTO';
+import { IUserRepository } from '../../../repository/IUserRepository';
+
+@injectable()
+export class CreateUserUseCase {
+    constructor(
+        @inject('UserRepository') private userRepository: IUserRepository,
+    ) {}
+
+    async execute({
+        name,
+        email,
+        password,
+        username,
+        driver_license,
+    }: ICreateUserDTO): Promise<void> {
+        await this.userRepository.create({
+            name,
+            email,
+            password,
+            username,
+            driver_license,
+        });
+    }
+}
