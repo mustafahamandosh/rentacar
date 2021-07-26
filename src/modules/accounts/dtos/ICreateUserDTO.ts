36013e0c8fc07b0b11ab8925064a697b0da6ca7a
mustafa <hamandoshm@hotmail.com>
2021-07-26 20:52:48 -0300

    Add user entity

---
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -0,0 +1,7 @@
+export interface ICreateUserDTO {
+    name: string;
+    email: string;
+    password: string;
+    username: string;
+    driver_license: string;
+}
--- src/modules/accounts/model/User.ts
+++ src/modules/accounts/model/User.ts
@@ -13,6 +13,9 @@ export class User {
     username: string;
 
     @Column()
+    email: string;
+
+    @Column()
     password: string;
 
     @Column()
