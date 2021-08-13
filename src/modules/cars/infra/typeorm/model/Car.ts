5f5cb5cb9d3af61bb7fe3db1014f99bb73f0c382
mustafa <hamandoshm@hotmail.com>
2021-08-13 13:53:43 -0300

    Add car entity

--- src/modules/cars/infra/typeorm/model/Car.ts
+++ src/modules/cars/infra/typeorm/model/Car.ts
@@ -1,31 +1,55 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryColumn,
+    JoinColumn,
+} from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
+import { Category } from 'models/Category';
+
+@Entity('cars')
 export class Car {
+    @PrimaryColumn()
     id?: string;
 
+    @Column()
     name: string;
 
+    @Column()
     description: string;
 
+    @Column()
     brand: string;
 
+    @Column()
     daily_rate: number;
 
+    @Column()
     license_plate: string;
 
+    @Column()
     fine_amount: number;
 
+    @Column()
     available: boolean;
 
+    @ManyToOne(() => Category)
+    @JoinColumn({ name: 'category_id' })
+    category: Category;
+
+    @Column()
     category_id: string;
 
+    @CreateDateColumn()
     created_at: Date;
 
     constructor() {
         if (!this.id) {
             this.id = uuid();
             this.available = true;
-            this.created_at = new Date();
         }
     }
 }
