0e81f03fae11e72944d7633d05d6290050ca766e
mustafa <hamandoshm@hotmail.com>
2021-08-13 13:55:38 -0300

    Refactor CreateCarController.ts

--- src/modules/cars/useCases/car/CreateCarController.ts
+++ src/modules/cars/useCases/car/CreateCarController.ts
@@ -1,1 +1,29 @@
-export class CreateCarController {}
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateCarUseCase } from 'usecases/car/CreateCarUseCase';
+
+export class CreateCarController {
+    async handle(req: Request, res: Response): Promise<Response> {
+        const {
+            name,
+            description,
+            brand,
+            daily_rate,
+            fine_amount,
+            license_plate,
+            category_id,
+        } = req.body;
+        const createCarUseCase = container.resolve(CreateCarUseCase);
+        const car = await createCarUseCase.execute({
+            name,
+            description,
+            brand,
+            daily_rate,
+            fine_amount,
+            license_plate,
+            category_id,
+        });
+        return res.status(201).json(car);
+    }
+}
