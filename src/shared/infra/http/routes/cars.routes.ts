76dd33a761dd23e35dfc07fd3d26384202b353f8
mustafa <hamandoshm@hotmail.com>
2021-08-20 18:33:24 -0300

    create list all available cars controller

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -3,9 +3,12 @@ import { Router } from 'express';
 import { isAdmin } from 'middlewares/isAdmin';
 import { isAuthenticated } from 'middlewares/isAuthenticated';
 import { CreateCarController } from 'usecases/car/createCar/CreateCarController';
+import { ListAvailableCarsController } from 'usecases/car/listAvaialbeCars/ListAvailableCarsController';
 
 export const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post('/', isAuthenticated, isAdmin, createCarController.handle);
+carsRoutes.get('/available', listAvailableCarsController.handle);
